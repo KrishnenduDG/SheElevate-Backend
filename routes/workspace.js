d4841d79f14f43aea5aebd9df69bb16ddb998191
John Doe <sumansingha8654@gmail.com>
2024-04-08 16:40:08 +0530

    add , delete , category route

--- routes/workspace.js
+++ routes/workspace.js
@@ -4,3 +4,23 @@ const router = express.Router();
 
 router.post("/add", (req, res) => {});
 export default router;
+function handleAdd(req, res) {
+    res.send('Response from /add route');
+}
+
+function handleDelete(req, res) {
+    res.send('Response from /delete route');
+}
+
+function handleCategory(req, res) {
+    res.send('Response from /?category route');
+}
+
+
+app.get('/add', handleAdd);
+app.get('/delete', handleDelete);
+app.get('/?category', handleCategory);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
