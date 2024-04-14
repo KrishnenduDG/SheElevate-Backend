7325357bd982714ddf52cc322a34a507b332d87a
KrishnenduDG <krishnendu.kdg@gmail.com>
2024-04-14 18:14:54 +0530

    -

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-app.get("/", firebaseAuth, regCheck, (req, res) => {
+app.get("/", (req, res) => {
   res.json({ status: true, msg: "Hello from Test Route.. Hi" });
 });
 
